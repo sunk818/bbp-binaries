0b8ccf0831c89ab4a762b3b8dfb05b951f55ed70
sunk818 <sunk818@users.noreply.github.com>
2019-09-28 22:48:05 -0700

    Create getbinaries.bat

---
+++ getbinaries.bat
@@ -0,0 +1,19 @@
+REM Windows
+
+wget -N --show-progress http://biblepay.org/biblepayevo32.exe
+wget -N --show-progress http://biblepay.org/biblepayevo64.exe
+
+REM LINUX
+
+wget -N --show-progress http://biblepay.org/biblepay-qt-evo-i686-pc-linux-gnu.tar.gz
+wget -N --show-progress http://biblepay.org/biblepay-qt-evo-x86_64-pc-linux-gnu.tar.gz
+
+wget -N --show-progress http://biblepay.org/biblepayd-evo-i686-pc-linux-gnu.tar.gz
+wget -N --show-progress http://biblepay.org/biblepayd-evo-x86_64-pc-linux-gnu.tar.gz
+
+wget -N --show-progress http://biblepay.org/biblepayd-evo-arm-linux-gnueabihf.tar.gz
+wget -N --show-progress http://biblepay.org/biblepayd-evo-aarch64-linux-gnu.tar.gz
+
+REM macOS
+
+wget -N --show-progress http://biblepay.org/biblepaycore-evo.dmg
