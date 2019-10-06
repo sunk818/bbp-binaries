3a8de90de8cc11ae10e0faa0cb4ab7750642e091
sunk818 <sunk818@users.noreply.github.com>
2019-10-05 22:11:45 -0700

    add testnet binaries

--- getbinaries.bat
+++ getbinaries.bat
@@ -3,7 +3,7 @@ REM Windows
 wget -N --show-progress http://biblepay.org/biblepayevo32.exe
 wget -N --show-progress http://biblepay.org/biblepayevo64.exe
 
-REM LINUX
+REM Linux
 
 wget -N --show-progress http://biblepay.org/biblepay-qt-evo-i686-pc-linux-gnu.tar.gz
 wget -N --show-progress http://biblepay.org/biblepay-qt-evo-x86_64-pc-linux-gnu.tar.gz
@@ -17,3 +17,26 @@ wget -N --show-progress http://biblepay.org/biblepayd-evo-aarch64-linux-gnu.tar.
 REM macOS
 
 wget -N --show-progress http://biblepay.org/biblepaycore-evo.dmg
+
+cd testnet
+
+REM testnet Windows
+
+wget -N --show-progress http://biblepay.org/biblepayevo32develop.exe
+
+REM testnet Linux
+
+wget -N --show-progress http://biblepay.org/biblepayd-evo-testnet-i686-pc-linux-gnu.tar.gz
+wget -N --show-progress http://biblepay.org/biblepay-qt-evo-testnet-i686-pc-linux-gnu.tar.gz
+
+wget -N --show-progress http://www.biblepay.org/biblepayd-evo-testnet-x86_64-pc-linux-gnu.tar.gz
+wget -N --show-progress http://biblepay.org/biblepay-qt-evo-testnet-x86_64-pc-linux-gnu.tar.gz
+
+wget -N --show-progress http://biblepay.org/biblepayd-evo-testnet-aarch64-linux-gnu.tar.gz
+wget -N --show-progress http://biblepay.org/biblepayd-evo-testnet-arm-linux-gnueabihf.tar.gz
+
+REM testnet macOS
+
+wget -N --show-progress http://biblepay.org/biblepaycore-evo-testnet.dmg
+
+cd ..
